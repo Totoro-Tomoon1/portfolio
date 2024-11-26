d5b0e799ae5cc0dd143e2a1082a74ec8c7d38994
Totoro-Tomoon1 <rboone@gaming.tech>
2024-11-26 18:18:32 +0100

    test

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+function add1(){
+    var txt=document.forms["form1"].somme1.value+="2€";
+    }
+    function add2(){
+    var txt=document.forms["form1"].somme1.value+="10€";
+    }
+    function add3(){
+    var txt=document.forms["form1"].somme1.value+="50€";
+    }
